b35d02f42979851889f79edce7b09f65c7d2442a
olegault <olegault@gwu.edu>
2022-12-13 22:25:58 +0000

    more db work

--- mysite/appstoreresults/views.py
+++ mysite/appstoreresults/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
+import sqlite3 as lite
 
 
 def index(request):
     return render(request, "index.html")
 
 def alphademo(request):
-    return render(request, "alphademo.html")
+    #connect to db 
+    try:
+        sqliteConnection = sqlite3.connect('apptable.db')
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
+        arr = []
+        result = cursor.execute('''SELECT title FROM apps''', arr)
+        sqliteConnection.commit()
+
+        #get all values
+        for row in result:
+            print(row)
+            
+        cursor.close()
+
+    #cannot connect:
+    except sqlite3.Error as error:
+        print("Failed to get data from sqlite table", error)
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+    results = apptable.db.objects.all()
+    return render(request, "alphademo.html", {'apps': results})
 
 def about(request):
     return render(request, "about.html")
