45612824497d2a3596a9e4c72a62f20d53a6f9bc
Rubin Roy <rubin.roy01@gmail.com>
2022-12-14 11:19:46 -0500

    Create clean-policies-readability.py

---
+++ clean-policies-readability.py
@@ -0,0 +1,40 @@
+from readabilipy import simple_json_from_html_string
+from urllib.parse import quote, unquote
+import utilities
+from database import Database
+import requests
+#basic readability lib from npm for js
+html_string = requests.get("https://enduco.app/en/privacy-policy-app/")
+
+html_string = utilities.get_raw_html(128)
+
+print(html_string)
+
+# with open('policyWithXvfb.html', 'r', encoding="utf-8", errors='ignore') as fp:
+#     html_string = fp.read()
+
+article = simple_json_from_html_string(html_string, use_readability=True)
+
+plain_text = ''
+
+for element in article['plain_text']:
+    plain_text += element['text'] + '\n'
+
+with open("cleaned.html", "w", encoding="utf-8", errors="ignore") as fp:
+    fp.write(plain_text)
+
+quoted_article = quote(plain_text, encoding='utf-8', errors='replace')
+with open("cleaned-quote.html", "w", encoding="utf-8", errors="ignore") as fp:
+    fp.write(quoted_article)
+
+unquoted_article = unquote(quoted_article, encoding='utf-8', errors='replace')
+with open("cleaned-unquote.html", "w", encoding="utf-8", errors="ignore") as fp:
+    fp.write(unquoted_article)
+
+with open("plaincleaned.html", "w", encoding="utf-8", errors="ignore") as fp:
+    fp.write(article['plain_content'])
+
+
+with open("plaintext.txt", "w", encoding="utf-8", errors="ignore") as fp:
+    for element in article['plain_text']:
+        fp.write(element['text'] + '\n\n')
