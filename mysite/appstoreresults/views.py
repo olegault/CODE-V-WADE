33e19a275e8764b1cdb1f8c286758109c03270c0
Miles Grant <milesgrantpk@gmail.com>
2023-02-15 12:33:28 -0500

    db integration stuff

--- mysite/appstoreresults/views.py
+++ mysite/appstoreresults/views.py
@@ -6,6 +6,7 @@ from .models import AppMetrics
 import numpy as np
 import sqlite3
 import cgi
+import json
 from .forms import SearchResult
 
 
@@ -20,18 +21,21 @@ def index(request):
         args=[]
         app_list =[]
         icon_list =[]
-        app_dict = {}
+        id_list = []
+
         #if(val=='y'):
-        sql = ("SELECT title, icon, rating FROM apps ORDER BY rating desc limit 10".format(seq=','.join(['?']*len(args))))
+        sql = ("SELECT title, icon, appID, rating FROM apps ORDER BY rating desc limit 10".format(seq=','.join(['?']*len(args))))
         rows = cursor.execute(sql, args)
         for row in rows:
+            print(row)
             icon_list.append(row[0]) #img
-            app_list.append(row[2]) #app name
+            app_list.append(row[3]) #app name
+            id_list.append(row[2])
 
-        template = loader.get_template('index.html')
+        # template = loader.get_template('index.html')
        
-        res = {app_list[i]: icon_list[i] for i in range(len(app_list))}
-
+        res = {app_list[i]: [icon_list[i], id_list[i]] for i in range(len(app_list))}
+        print(res)
         return render(request, 'index.html', {'res':res})
 
     #cannot connect:
@@ -57,7 +61,7 @@ def scorecard(request):
     context = {'date': 'Feb 7, 2023',
                'title': "Clue Period & Cycle Tracker",
                'downloads': '10M+ downloads',
-               'appIcon': 'Hey!',
+               'appIcon': '../static/media/clue-icon.png',
                'overallScore': 83,
                'thirdPartyScore': 80,
                'dataEncryptionScore': 75,
@@ -65,6 +69,54 @@ def scorecard(request):
                'transparencyScore': 50}
 
     return HttpResponse(template.render(context, request))
+
+def scorecard(request, appID=None):
+    # data = AppM3.objects.all()
+
+    template = loader.get_template("scorecard.html")
+    context = {'date': 'Feb 7, 2023',
+               'title': "Clue Period & Cycle Tracker",
+               'downloads': '10M+ downloads',
+               'appIcon': '../static/media/clue-icon.png',
+               'overallScore': 83,
+               'thirdPartyScore': 80,
+               'dataEncryptionScore': 75,
+               'sensitiveDataScore': 100,
+               'transparencyScore': 50}
+
+    try:
+        sqliteConnection = sqlite3.connect('apptable.db')
+        sqliteConnection.row_factory = sqlite3.Row
+        cursor = sqliteConnection.cursor()
+        print("Successfully Connected to SQLite")
+
+        cursor = sqliteConnection.execute("SELECT * FROM apps WHERE appID like ?", ("%" + appID + "%",))
+        res = cursor.fetchall()
+        
+        if len(res) != 0:
+            print(dict(res[0]))
+            app = res[0]
+            context = {'date': 'Feb 7, 2023',
+                        'title': app['title'],
+                        'downloads': f"{app['downloads']} downloads",
+                        'appIcon': app['icon'],
+                        'overallScore': 83,
+                        'thirdPartyScore': 80,
+                        'dataEncryptionScore': 75,
+                        'sensitiveDataScore': 100,
+                        'transparencyScore': 50}
+            return HttpResponse(template.render(context, request))
+                
+                
+        else:
+            res = "App not found, try again"
+            return HttpResponse(template.render(context, request))
+
+    #cannot connect:
+    except sqlite3.Error as error:
+        print("Failed to connect", error)
+        str = "app not found"
+        return HttpResponse(template.render(context, request))
 
 
 def search(request):
@@ -79,19 +131,21 @@ def search(request):
                 cursor = sqliteConnection.cursor()
                 print("Successfully Connected to SQLite")
 
-                cursor = sqliteConnection.execute("SELECT title, icon FROM apps WHERE title LIKE ?", ("%" + query + "%",))
+                cursor = sqliteConnection.execute("SELECT title, icon, appID FROM apps WHERE title LIKE ?", ("%" + query + "%",))
                 res = cursor.fetchall()
                 
                 if len(res) != 0:
                     app_list =[]
                     icon_list =[]
+                    id_list = []
 
                     for r in res:
                         icon_list.append(r[0]) 
                         app_list.append(r[1])
-                        output = {app_list[i]: icon_list[i] for i in range(len(app_list))}
+                        id_list.append(r[2])
+                        output = {app_list[i]: [icon_list[i], id_list[i]] for i in range(len(app_list))}
                     
-                    return render(request, 'search.html', {'output':output})
+                    return render(request, 'search.html', {'output': output})
                         
                         
                 else:
@@ -112,6 +166,4 @@ def search(request):
 
 def submit(request):
     return render(request, "submit.html")
-
-
 
