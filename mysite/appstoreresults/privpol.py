5393680522799d070faf68a59d99f3c743149fff
Rubin Roy <rubin.roy01@gmail.com>
2023-03-04 22:51:48 -0500

    arr ret comment

--- mysite/appstoreresults/privpol.py
+++ mysite/appstoreresults/privpol.py
@@ -1,8 +1,10 @@
 import io
+import sys
+import StringIO
 #import csv
 #import time
 #import pandas as pd
-#import requests
+import requests
 import get_top_apps
 from google_play_scraper import app
 #from policy-scraping import get_policy
@@ -25,6 +27,24 @@ r = requests.post(
     },
     headers={'api-key': '62094660-0461-4c8a-9885-7243213f3b81'}
 )
+
+text = r.txt
+arr = []#delte data
+if "identifying information" in text:
+    arr[0]=1
+if "non-reproductive" in text:
+    arr[1]=1
+if "medication" in text:
+    arr[2]=1
+if "period calendar" in text:
+    arr[3]=1
+if "deletion" in text:
+    arr[4]=1
+if "control" in text:
+    arr[5]=1
+if "control" and "shared" in text:
+    arr[6]=1
+#ret arr
 print(r.json())
 
 
